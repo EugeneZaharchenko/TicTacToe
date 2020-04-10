87164e8c953750bfd5eda505bad2bd249a4b0492
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2020-04-10 10:12:32 +0300

    static css added

--- gameplay/models.py
+++ gameplay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 GAME_STATUS_CHOICES = (
@@ -8,6 +9,17 @@ GAME_STATUS_CHOICES = (
     ('L', 'Second player wins'),
     ('D', 'Draw'),
 )
+
+class GamesQuerySet(models.QuerySet):
+    def games_for_user(self, user):
+        return self.filter(
+            Q(first_player=user) | Q(second_player=user)
+        )
+
+    def active(self):
+        return self.filter(
+            Q(status='F') | Q(status='S')
+        )
 
 class Game(models.Model):
     first_player = models.ForeignKey(User,
@@ -20,6 +32,8 @@ class Game(models.Model):
 
     status = models.CharField(max_length=1, default='F',
                               choices=GAME_STATUS_CHOICES)
+
+    objects = GamesQuerySet.as_manager()
 
     def __str__(self):
         return "{0} vs {1}".format(
