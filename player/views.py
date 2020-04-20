0b88ea0dc57ab3a21af54ff6c4fcf73425093884
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2020-04-20 19:45:21 +0300

    new_invitation form was added

--- player/views.py
+++ player/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from gameplay.models import Game
-
+from .forms import InvitationForm
 
 @login_required
 def home(request):
@@ -10,3 +10,9 @@ def home(request):
 
     return render(request, "player/home.html",
                   {'games': active_games})
+
+
+@login_required
+def new_invitation(request):
+    form = InvitationForm()
+    return render(request, 'player/new_invitation_form.html', {'form': form})
