f22f44dad7b693cc704a981933a5b5ed1b568729
J0nnyZ <EugeneZaharchenko@users.noreply.github.com>
2020-04-20 18:17:48 +0300

    Login/logout realized

--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '+30(f)g8!tbqmkc_x*gafa46dgy^y@@zabu#=t7ws%59cnmg$^'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -70,12 +68,7 @@ TEMPLATES = [
     },
 ]
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
-
 WSGI_APPLICATION = 'TicTacToe.wsgi.application'
-
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
@@ -86,7 +79,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -106,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -120,7 +111,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
@@ -129,3 +119,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+LOGIN_REDIRECT_URL = 'player_home'
+LOGOUT_REDIRECT_URL = 'tictactoe_welcome'
+LOGIN_URL = 'player_login'
--- player/urls.py
+++ player/urls.py
@@ -4,6 +4,6 @@ from .views import home
 
 urlpatterns = [
     path('home/', home, name='player_home'),
-    path('login/', LoginView.as_view(template_engine='player/login_form.html'), name='player_login'),
-    path('logout/', LoginView.as_view(), name='player_logout'),
+    path('login/', LoginView.as_view(template_name='player/login_form.html'), name='player_login'),
+    path('logout/', LogoutView.as_view(), name='player_logout'),
 ]
